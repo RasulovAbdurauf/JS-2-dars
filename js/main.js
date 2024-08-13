009e71266b0cad9c2fb92966873176e5f6bcd818
RasulovAbdurauf <abduraufrasuluzb001@gmail.com>
2024-08-13 14:01:31 +0500

    JS 2-dars
    Zeromax IT-Center, Web programming 36-group

---
+++ js/main.js
@@ -0,0 +1,112 @@
+// JSga kirish
+//console.log("Men faqat konsol oynasida ko'rinaman!");
+//alert("Saytimizga xush kelibsiz!");
+//document.write("Men bodyda ko'rinaman!");
+// JS o'zgaruvchilari
+//let x = 5;
+//let b = 47;
+//let x = 15, y = 7, z = 10;
+//number
+//string
+//let son = 45;
+//let matn ="JavaScript"
+//document.write( typeof matn);
+//let x = 714, b = 41, j = 2, like = 98589, job =14;
+//document.write(714*41*2-98589/14)
+// JS matematika 
+//let x = 78;
+//x += 6;     x=5+5    qo'shish
+//x -= 5;     x=5-5    ayirish
+//x *= 5;     x=5*5    ko'paytirish
+//x /= 2;     x=5/5    bo'lish
+//let qoldiq =x%12;    qoldiq
+//let daraja =x**5;    daraja
+//++x;
+//--x;
+//x++;
+//x--;
+//document.write(x);
+
+//if ("salom" == "salom"){
+//   console.log("Ular teng!")
+//}
+//else{
+//    console.log("Ular teng emas!")
+//}
+
+//if(44 >= 45){
+//   console.log("Ular teng yoki katta")
+//}
+//else{
+//    console.log("Ular teng emas yoki kichik")
+//}
+
+//if(46 <= 45){
+//   console.log("Solishtirayotgan son 45ga teng yoki katta!")
+//}
+//else{
+//    console.log("Solishtirayotgan son 46ga teng emas yoki kichik!")
+//
+//}
+
+//if(4 - 45){
+//    console.log("Ular teng!")
+// }
+// else{
+//     console.log("Ular teng emas!")
+// }
+ 
+//if('black' == 'white'){
+//    console.log("Ular teng!")
+//}
+//else{
+//    console.log("Ular teng emas!")
+//}
+
+//if(5656 === 7887){
+//    console.log("Benzin!")
+//}
+//else if(2121 == 5678){
+//    console.log("Propan")
+//}
+//else{
+//    console.log("Metan")
+//}
+
+//let a =46;
+//let b =10;
+//let c =19;
+//let d =49;
+
+//if(a == 46 && c == 19 && d == 49){
+//    console.log("---")
+//}
+//else{
+//    console.log("____")
+//}
+
+//let a =77;
+//let b =77;
+//let c =77;
+//let d =77;
+//if(a == 46 && b == 10 && c == 19 && d == 49){
+//    console.log("Shartlarning hamasi togri!")
+//}
+//else{
+//    console.log("Shartlarning hamasi yokiy kamida bitasi notog'ri!")
+//}
+
+//if(a == 46 || b == 10 || c == 19 || d == 49){
+//    console.log("Shartlarning hamasi yoki kamida bittasi to'ri");
+//}
+//else{
+//    console.log("Shartlarning hammasi noto'gri");//
+//}
+
+
+//if(a == 77 && b == 77 && c == 77 && d == 77){
+//    console.log("Shartlarning hamasi togri!")
+//}
+//else{
+//    console.log("Shartlarning hamasi yokiy kamida bitasi notog'ri!")
+//}
